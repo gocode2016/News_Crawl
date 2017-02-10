d1bffafa879603c22dcdc0b98154dc600d44e0b3
sakura <392285478@qq.com>
2017-02-10 16:47:27 +0800

    爬虫完成

--- crawl.py
+++ crawl.py
@@ -9,29 +9,30 @@ class Crawl(object):
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
     
-    def craw(self, root_url):
+    def craw(self, root_urls):
         count = 1
-        self.urls.add_new_url(root_url)
-        while self.urls.has_new_url():
-            try:
-                new_url = self.urls.get_new_url()
-                print 'craw %d : %s' % (count, new_url)
-                html_cont = self.downloader.download(new_url)
-                new_urls, new_data = self.parser.parse(new_url, html_cont)
-                self.urls.add_new_urls(new_urls)
-                self.outputer.collect_data(new_data)
-                count = count + 1
-            except Exception,e:
-                print 'craw failed!'
-                print 'str(Exception):\t', str(Exception)
-                print 'str(e):\t\t', str(e)
-                print 'repr(e):\t', repr(e)
-                print 'e.message:\t', e.message
-                print 'traceback.print_exc():'; traceback.print_exc()
-                print 'traceback.format_exc():\n%s' % traceback.format_exc()
+        for root_url in root_urls:
+            self.urls.add_new_url(root_url)
+            while self.urls.has_new_url():
+                try:
+                    new_url = self.urls.get_new_url()
+                    print 'craw %d : %s' % (count, new_url)
+                    html_cont = self.downloader.download(new_url)
+                    new_urls, new_data = self.parser.parse(new_url, html_cont)
+                    self.urls.add_new_urls(new_urls)
+                    self.outputer.collect_data(new_data)
+                    count = count + 1
+                except Exception,e:
+                    print 'craw failed!'
+                    print 'str(Exception):\t', str(Exception)
+                    print 'str(e):\t\t', str(e)
+                    print 'repr(e):\t', repr(e)
+                    print 'e.message:\t', e.message
+                    print 'traceback.print_exc():'; traceback.print_exc()
+                    print 'traceback.format_exc():\n%s' % traceback.format_exc()
         self.outputer.output_html()
 
 if __name__=="__main__":
-    root_url = "http://cs.cqut.edu.cn/Notice/NoticeStudentMore.aspx"
+    root_urls = ["http://cs.cqut.edu.cn/Notice/NoticeStudentMore.aspx", "http://cs.cqut.edu.cn/Notice/NoticeMore.aspx?NtcCategoryID=5", "http://cs.cqut.edu.cn/News/NewsMore.aspx", "http://cs.cqut.edu.cn/Notice/NoticeEmpMore.aspx"]
     obj_News_crawl = Crawl()
-    obj_News_crawl.craw(root_url)
+    obj_News_crawl.craw(root_urls)
--- html_parser.py
+++ html_parser.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import re
 import urlparse
+import datetime
 
 class HtmlParser(object):
 
@@ -9,23 +10,31 @@ class HtmlParser(object):
     def _get_new_urls(self, page_url, soup):
         new_urls = set()
         #/CommonInfo/CommonInfoContent.aspx?CommonInfoID=406
-        links = soup.find_all('a', href=re.compile(r"\.\./CommonInfo/CommonInfoContent.aspx\?CommonInfoID=\d+"))
+        #<a id="ctl00_ContentPlaceHolder_main_NoticeStudentMore1_GridView1_ctl10_HyperLink4"
+        #<a id="ctl00_ContentPlaceHolder_main_NoticeMore1_GridView1_ctl02_HyperLink3"
+        #<a id="ctl00_ContentPlaceHolder_main_NewsMore1_GridView1_ctl02_HyperLink3"
+        #<a id="ctl00_ContentPlaceHolder_main_EmpMore1_GridView1_ctl02_HyperLink4"
+        links = soup.find_all('a', id=re.compile(r"ctl00_ContentPlaceHolder_main_\w+_GridView1_ctl\d+_HyperLink[3-4]"))
+        
         for link in links:
-            print link.get_text()
+            #print link.get_text()
             new_url = link['href']
             new_full_url = urlparse.urljoin(page_url, new_url)
-            print new_full_url
-            new_urls.add(new_full_url)
+            if link.get_text() == datetime.datetime.now().strftime("%m/%d"):
+            #if link.get_text() == '12/02':
+                new_urls.add(new_full_url)
         return new_urls
 
     def _get_new_data(self, page_url, soup):
         res_data = {}
         count = 1
+
         #<p class="MsoNormal" 
         summary_nodes = soup.find_all('p')
         for summary_node in summary_nodes:
             res_data[count] = summary_node.get_text()
             count = count + 1
+
         return res_data
 
     def parse(self, page_url, html_cont):
@@ -35,5 +44,4 @@ class HtmlParser(object):
         soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
         new_urls = self._get_new_urls(page_url, soup)
         new_data = self._get_new_data(page_url, soup)
-        print new_data
         return new_urls, new_data
